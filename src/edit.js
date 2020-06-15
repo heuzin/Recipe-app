75fedec23fe402f679138dfa970f04307ff03958
heuzin <matheusqs.arq@gmail.com>
2020-06-15 17:27:59 -0400

    creating function for button create Steps

--- src/edit.js
+++ src/edit.js
@@ -3,6 +3,7 @@ import { updateRecipe, removeRecipe } from "./recipes"
 
 const titleElement = document.querySelector('#note-title')
 const removeElement = document.querySelector('#remove-recipe')
+const addStepsElement = document.querySelector('#add-steps')
 const recipeId = location.hash.substring(1)
 
 initializeEditPage(recipeId)
@@ -16,6 +17,11 @@ titleElement.addEventListener('input', (e) => {
 removeElement.addEventListener('click', (e) => {
     removeRecipe(recipeId)
     location.assign('/index.html')
+})
+
+addStepsElement.addEventListener('click', (e) => {
+    const id = createSteps()
+    location.assign(`/steps.html`)
 })
 
 window.addEventListener('storage', (e) => {
