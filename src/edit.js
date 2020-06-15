adedaa7e30ad5aaa5e36b4c14f82bf68d7a55aca
heuzin <matheusqs.arq@gmail.com>
2020-06-15 17:15:50 -0400

    Remove recipe button working

--- src/edit.js
+++ src/edit.js
@@ -1,7 +1,8 @@
 import { initializeEditPage } from "./views"
-import { updateRecipe } from "./recipes"
+import { updateRecipe, removeRecipe } from "./recipes"
 
 const titleElement = document.querySelector('#note-title')
+const removeElement = document.querySelector('#remove-recipe')
 const recipeId = location.hash.substring(1)
 
 initializeEditPage(recipeId)
@@ -10,6 +11,11 @@ titleElement.addEventListener('input', (e) => {
     const recipe = updateRecipe(recipeId, {
         title: e.target.value
     })
+})
+
+removeElement.addEventListener('click', (e) => {
+    removeRecipe(recipeId)
+    location.assign('/index.html')
 })
 
 window.addEventListener('storage', (e) => {
